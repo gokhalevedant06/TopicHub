fa021de45b688e7e8aae45c227bbea93d93f9dba
gokhalevedant06 <gokhalevedant06@gmail.com>
2022-04-01 23:10:46 +0530

    Database connection done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const port =  process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+//using middleware to parse json data
+app.use(express.json());
+
+require("./db/conn");
+// app.use("/", require("./routes/UserRoutes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port http://localhost:${port}`);
+});
