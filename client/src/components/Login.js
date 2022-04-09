df1ef331bc2c70e3f818ec7a49d3000b40ff1a56
Aayush <aayushpdkv@gmail.com>
2022-04-09 11:47:09 +0530

    added login signup components and landing pags

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import {
+  Box, Flex, Heading, Input, Button, useColorMode, useColorModeValue, Checkbox,
+   Link, InputGroup,  InputRightElement, 
+} from '@chakra-ui/react'
+import { EmailIcon, UnlockIcon, MoonIcon, SunIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { useState } from "react";
 
 const Login = () => {
+  const [show, setShow] = React.useState(false)
+  const handleClick = () => setShow(!show)
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [showPassword, setShowPassword] = useState(false);
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+  const pageBackground = useColorModeValue("blue.100", "gray.600");
   return (
-    <div>Login</div>
+
+    <Flex background={pageBackground} direction="column" height="100vh" alignItems='center' justifyContent='center'>
+      <Flex width="70vh" direction="column" background={formBackground} p={10} rounded={6}>
+        <Heading mb={6}> LOGIN</Heading>
+
+        {/* Email Input */}
+        <Flex alignItems='center'  >
+          <EmailIcon />
+          <Input ml={5} placeholder="Enter Email ID" varient="filled" mb={3} type="email" />
+        </Flex>
+
+        <Flex alignItems='center'>
+          <UnlockIcon />
+          <InputGroup size='md'>
+            {/* Password Input */}
+            <Input ml={5} mb={5}
+              pr='4.5rem'
+              type={show ? 'text' : 'password'}
+              placeholder='Enter password'
+            />
+            <InputRightElement width='4.5rem'>
+              <Button h='1.75rem' size='sm' onClick={handleClick}>
+                {show ? <ViewOffIcon /> : <ViewIcon />}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
+
+        <Link href='https://chakra-ui.com' textAlign="right">forgot password?</Link>
+
+        <Checkbox defaultChecked mb={5}>Remember my credentials.</Checkbox>
+
+        <Button mb={6} colorScheme='teal'>Sign in</Button>
+
+        <Button w={10} colorScheme='red' onClick={toggleColorMode}>
+          {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}</Button>
+      </Flex>
+
+      <Box>
+        New to us?{" "}
+        <Link color="teal.500" href="http://localhost:3000/teacher/Signup">
+          Sign Up
+        </Link>
+      </Box>
+
+    </Flex>
   )
 }
+
 
 export default Login
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,68 @@
 import React from 'react'
-
+import { Flex, Box, Text, Link, Image,extendTheme } from '@chakra-ui/react'
 const Landing = () => {
   console.log("LANDING")
+  const PageBackground = {
+    background:
+      'url(https://c4.wallpaperflare.com/wallpaper/11/624/62/simple-desk-clocks-books-wallpaper-preview.jpg) center/cover no-repeat',
+    // bgGradient: 'linear(red.100 0%, orange.100 25%, yellow.100 50%)',
+
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+  const ButtonStyle = {
+    as: 'button',
+    p: '20px',
+    color: 'white',
+    fontWeight: 'bold',
+    borderRadius: 'full',
+    bgGradient: 'linear(to-r, blue.500, blue.500)'
+  }
   return (
-    <div>Landing Page</div>
+    <Flex w='100%'
+      h='100vh'
+      sx={PageBackground}
+      direction='column'
+      mb={50} >
+
+      <Box alignItems='center' justifyContent='center' pl='300px' w='80%' h='300px' borderRadius="xl" bgColor='rgba(0,0,0, 0.4)' >
+
+        <Text
+          bgGradient='linear(to-l, blue.200,blue.200)'
+          bgClip='text'
+          fontSize='7xl'
+          alignItems='center'
+          textAlign='center'
+          mr={270}
+          fontWeight='extrabold'>
+          Topic Hub
+        </Text>
+
+        <Flex alignItems='center' ml='0px' direction='row'>
+
+          <Box
+            sx={ButtonStyle}
+            mt={10}
+            _hover={{
+              bgGradient: 'linear(to-r, red.300, red.300)',
+            }} >
+            <Link href="http://localhost:3000/teacher/Login" >Teachers Login</Link>
+          </Box>
+
+          <Box
+            sx={ButtonStyle}
+            mt={10}
+            ml={150}
+            _hover={{
+              bgGradient: 'linear(to-r, red.300, red.300)',
+            }} >
+            <Link textDecoration='none' href="http://localhost:3000/teacher/Login" >Students Login</Link>
+          </Box>
+
+        </Flex>
+      </Box>
+    </Flex>
   )
 }
 
