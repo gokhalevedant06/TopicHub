d094f84e47a1c91e6371a8d884a07a4cf81fed85
Adwait Gharpure <adwaitgharpure@gmail.com>
2022-05-30 18:22:20 +0530

    Modified Student Profile, bg color remaining

--- client/src/pages/StudentProfile.js
+++ client/src/pages/StudentProfile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { useNavigate } from 'react-router-dom';
 import {Flex, Box, Text, Button, Image} from '@chakra-ui/react'
@@ -7,65 +8,48 @@ const StudentProfile = () => {
   const navigate = useNavigate();
   return (
     <>
-      <Flex wrap={'wrap'} justifyContent={'center'} >
-<Flex  wrap={"wrap"} alignItems={'center'} justifyContent={"start"} mt="7rem"  backgroundColor={"#9999ff"} 
-height={"250px"} w={"80%"} >
-    <Box width={"300px"} ml="10%" >
-      <Image 
-      boxSize='200px'
-      objectFit={'cover'}
-      src={dp} 
-      // width={"300px"}  
-      alt={"dp"}
-      borderRadius={"full"}>
+      <Flex wrap={'wrap'} justifyContent={'center'} flexDirection={'row'}>
+      <Flex  wrap={"wrap"} alignItems={'center'} justifyContent={"start"} mt="7rem"  backgroundColor={"#9999ff"} 
+      height={"50px"} w={"90%"} >
+      <Text fontSize={"30px"} textColor={'#FFFFFF'} ml = {"5rem"} fontFamily={'Roboto'}>My Profile</Text>
+        </Flex>
+      <Flex  wrap={"wrap"} alignItems={'center'} justifyContent={"start"} mt="3rem"  backgroundColor={"#9999ff"} 
+      height={"300px"} w={"90%"} >
+      <Box width={"300px"} ml="10%" >
+        <Image 
+        boxSize='200px'
+        objectFit={'cover'}
+        src={dp} 
+        // width={"300px"}  
+        alt={"dp"}
+        borderRadius={"full"}>
       
-      </Image>
-    </Box>
-    <Box width={"300px"} margin="10px" >
-        <Text padding={"2px"}>Name: </Text>
-        <Text padding={"2px"}>Email: </Text>  
-        <Text padding={"2px"}>Phone: </Text>
-        <Text padding={"2px"}></Text>
+        </Image>
+      </Box>
+      <Box width={"300px"} margin="10px" >
+        <Text padding={"2px"}> <b>Name:</b> Adwait Gharpure</Text>
+        <Text padding={"2px"}><b>Email:</b> adwait.gharpure20@vit.edu</Text>  
+        <Text padding={"2px"}><b>Phone:</b> 9545831983</Text>
+        <Text padding={"2px"}><b>Branch: </b> Artificial Intelligence and Data Science</Text>
         
-    </Box>
-    <Box >
-    <Text padding={"2px"}>PRN No:</Text>
-    <Text padding={"2px"}>Roll No: </Text>  
-    <Text padding={"2px"}>Division: </Text>
+      </Box>
+        <Box width={"300px"} margin="10px" >
+        <Text padding={"2px"}><b>PRN No:</b>12010440</Text>
+        <Text padding={"2px"}><b>Roll No:</b> 36</Text>  
+        <Text padding={"2px"}><b>Division: </b>AI-A</Text>
+      
+      </Box>
+  </Flex>
+      <Flex>
     
-    </Box>
-</Flex>
-</Flex>
-    <Box width={"70%"} marginLeft="auto" marginRight={"auto"}>
-
-            <Text textAlign={"center"}>
-              Class Details
-            </Text>
-            <Box marginLeft={"5rem"}>
-              <Text>
-                Title: 
-              </Text>
-              <Text>
-                Description: 
-              </Text>
-              
-            </Box>
-            <Flex>
-              {/* <Box>
-                Image
-              </Box> */}
-              <Flex flexDirection={"column"} align="center" width={"80%"}>
-                <Box>
-              <Button onClick={()=>navigate('/student/classSection')} m={"1rem"} w={"200px"} >My Class</Button>
-              {/* <Button m={"1rem"} w={"200px"} >My Teachers </Button> */}
-
-                </Box>
-                <Box>
-
-                </Box>
-              </Flex>
-            </Flex>
-    </Box>
+        <Flex flexDirection={"column"} align="center" width={"80%"}>
+          <Box>
+        <Button onClick={()=>navigate('/student/classSection')} m={"1rem"} w={"200px"} ><b>My Class</b></Button>
+          </Box>
+        </Flex>
+      </Flex>
+  </Flex>
+    
     </>
   )
 }
