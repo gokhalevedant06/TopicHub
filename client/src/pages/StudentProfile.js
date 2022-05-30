a92fecc4000b638e3553fd7da3218ab6201fb41f
Adwait Gharpure <adwaitgharpure@gmail.com>
2022-05-30 14:09:51 +0530

    Added student dashboard

--- client/src/pages/StudentProfile.js
+++ client/src/pages/StudentProfile.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { useNavigate } from 'react-router-dom';
-import {Flex, Box, Text, Button} from '@chakra-ui/react'
+import {Flex, Box, Text, Button, Image} from '@chakra-ui/react'
+import dp from '../Assets/Images/profilePic.jpg';
 const StudentProfile = () => {
   const navigate = useNavigate();
   return (
     <>
       <Flex wrap={'wrap'} justifyContent={'center'} >
-<Flex  wrap={"wrap"} alignItems={'center'} justifyContent={"center"} mt="7rem"  backgroundColor={"#9999ff"} height={"250px"} w={"80%"} >
-    <Box width={"300px"} margin="10px" >Image</Box>
+<Flex  wrap={"wrap"} alignItems={'center'} justifyContent={"start"} mt="7rem"  backgroundColor={"#9999ff"} 
+height={"250px"} w={"80%"} >
+    <Box width={"300px"} ml="10%" >
+      <Image 
+      boxSize='200px'
+      objectFit={'cover'}
+      src={dp} 
+      // width={"300px"}  
+      alt={"dp"}
+      borderRadius={"full"}>
+      
+      </Image>
+    </Box>
     <Box width={"300px"} margin="10px" >
-        <Text padding={"2px"}>Name:</Text>
+        <Text padding={"2px"}>Name: </Text>
         <Text padding={"2px"}>Email: </Text>  
         <Text padding={"2px"}>Phone: </Text>
+        <Text padding={"2px"}></Text>
+        
     </Box>
-    <Box  >
-    <Text padding={"2px"}>PRN No.:</Text>
-        <Text padding={"2px"}>Roll No. </Text>  
-        <Text padding={"2px"}>Division </Text>
+    <Box >
+    <Text padding={"2px"}>PRN No:</Text>
+    <Text padding={"2px"}>Roll No: </Text>  
+    <Text padding={"2px"}>Division: </Text>
+    
     </Box>
 </Flex>
 </Flex>
