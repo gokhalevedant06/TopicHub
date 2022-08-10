80143264cfc8864850b6e3f3880fc1be7e029acc
Vedant Gokhale <gokhalevedant06@pop-os.localdomain>
2022-08-10 17:10:13 +0530

    Config

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ require("./db/conn");
 app.use("/teacher", require("./routes/teacherRoutes"));
 app.use("/student", require("./routes/studentRoutes"));
 
+if(process.env.NODE_ENV=='production'){
+  app.use(express.static('client/build'));
+  const path = require('path')
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
 });
