7442bfc8dc0f50f2e4bb21d6c9a32e7261c6b059
Vedant Gokhale <gokhalevedant06@gmail.com>
2022-08-10 17:40:48 +0530

    Update Signup.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -14,7 +14,7 @@ import { useSnackbar } from 'notistack';
 const Signup = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const [isTeacher,setTeacher] = useState(false);
-  if(window.location.href[22]=='t' && isTeacher==false) setTeacher(true)
+  if(window.location.href[32]=='t' && isTeacher==false) setTeacher(true)
   console.log(window.location.href,"URL")
   console.log(window.location.href[33],"URL")
   const [show, setShow] = useState(false)
